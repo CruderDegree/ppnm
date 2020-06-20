598152eb02365419de732a2d0042d315dbd87ad8
CruderDegree <tobi2109@gmail.com>
2020-06-20 22:22:13 +0200

    Added Problem 8 B and C

--- math/Minimization.cs
+++ math/Minimization.cs
@@ -1,4 +1,6 @@
 using System;
+using static System.Console;
+using System.Collections.Generic;
 using static System.Math;
 
 public class Minimization{
@@ -60,5 +62,104 @@ public class Minimization{
 		matrix B = new matrix(n,n);
 		for(int i = 0; i < n; i++) B[i,i] = 1;
 		return B;
+	}
+
+	public class DownhillSimplex{
+		public int steps{get;}
+		private int n, h = 0, l = 0;
+		private List<vector> simplex = new List<vector>();
+		private double tol;
+		private Func<vector, double> f;
+		private vector centroid;
+		public vector x{get;}
+		// Solution when method done
+
+		public DownhillSimplex(Func<vector, double> f, vector x0, double eps){
+			this.f = f;
+			this.tol = eps;
+			this.n = x0.size;
+			this.x = x0;
+			this.steps = 0;
+			makeSimplex();
+			do{
+				steps++;
+				updateSimplex();
+				vector reflected = reflection();
+				if(f(reflected) < f(simplex[l])){
+					vector expanded = expansion();
+					if(f(expanded) < f(reflected)) simplex[h] = expanded;
+					else simplex[h] = reflected;
+				}
+				else{
+					if(f(reflected) < f(simplex[h])) simplex[h] = reflected;
+					else{
+						vector contracted = contraction();
+						if(f(contracted) < f(simplex[h])) simplex[h] = contracted;
+						else reduction();
+					}
+				}
+			}while(size() > tol);
+			this.x = simplex[l];
+		}
+
+		private void makeSimplex(){
+			simplex.Add(x.copy());
+			for(int i = 0; i < n; i++){
+				vector x0 = x.copy();
+				x0[i] = 5*x[i] + 5;
+				simplex.Add(x0);
+			}
+		}
+
+		private void updateSimplex(){
+			// Update h and l
+			for(int i = 0; i < n + 1; i++){
+				vector p = simplex[i];
+				if(f(p) > f(simplex[h])) h = i;
+				if(f(p) < f(simplex[l])) l = i;
+			}
+			// Calculate centroid
+			centroid = new vector(n);
+			for(int i = 0; i < n + 1; i++) if(i!=h){
+				centroid += simplex[i];
+			}
+			centroid /= n;
+		}
+
+		private vector reflection(){
+			return centroid + (centroid - simplex[h]);
+		}
+
+		private vector expansion(){
+			return centroid + 2*(centroid - simplex[h]);
+		}
+
+		private vector contraction(){
+			return centroid + 0.5*(simplex[h] - centroid);
+		}
+
+		private void reduction(){
+			for(int k = 0; k < n+1; k++) if(k != l){
+				simplex[k] = 0.5*(simplex[k] + simplex[l]);
+			}
+		}
+
+		private double size(){
+			double s = 0;
+			for(int k = 0; k < n + 1; k++) if(k != l){
+				double dist = distance(k);
+				if(dist > s) s = dist;
+			}
+			return s;
+		}
+
+		private double distance(int k){
+			double s = 0;
+			for(int i = 0; i < n; i++){
+				s += Pow(simplex[l][i] - simplex[k][i], 2);
+			}
+			return Sqrt(s);
+		}
+		
 	}
 }
--- problems/08-minimization/main.cs
+++ problems/08-minimization/main.cs
@@ -1,5 +1,7 @@
+using System.IO;
 using static System.Console;
 using static System.Math;
+using System.Collections.Generic;
 using static Minimization;
 using System;
 
@@ -29,5 +31,53 @@ class main{
 		p.print("Starting point:");
 		roots.print($"Minimum found ({steps} steps)");
 		WriteLine("Local minimum: (3,2)");
+
+		WriteLine("\n\n--- Part B ---");
+		// Read higgs.txt for fitting data
+		StreamReader Reader = new StreamReader("higgs.txt");
+		int n = 30;
+		vector E = new vector(n), sigma = new vector(n), err = new vector(n);
+		for(int i = 0; i < n; i++){
+			string line = Reader.ReadLine();
+			string [] data = line.Split(new char [] {' '});
+			E[i] = (Double.Parse(data[0]));
+			if(data[1] == ""){
+				sigma[i] = Double.Parse(data[2]);
+				err[i] = Double.Parse(data[3]);
+			}
+			else{ 
+				sigma[i] = (Double.Parse(data[1]));
+				err[i] = (Double.Parse(data[2]));
+			}
+		}
+		Reader.Close();
+ 
+		Func<double, double, double, double, double> breitWigner = (e, m, g, a) =>
+			a/(Pow(e-m,2) + g*g/4);
+
+		Func<vector, double> deviation = delegate(vector z){
+			double m = z[0];
+			double gamma = z[1];
+			double A = z[2];
+			double sum = 0;
+			for(int i = 0; i < n; i++){
+				sum += Pow(breitWigner(E[i], m, gamma, A) - sigma[i], 2);
+			}
+			return sum;
+		};
+
+		p = new vector(125, 2, 6);
+		vector min;
+		(min, steps) = qNewton(deviation, p, eps);
+		WriteLine($"Fitting Higgs data to Breit-Wigner function, done in {steps} steps.");
+		WriteLine($"Found parameters:");
+		WriteLine($"Mass: {min[0]} \nGamma: {min[1]} \nA: {min[2]}");
+
+		WriteLine("\n\n--- Part C ---");
+		WriteLine("Find minimum Rosenbrock Valley function...");
+		p = new vector(0.0, 0.0);
+		eps = 1e-3;
+		DownhillSimplex DHS = new DownhillSimplex(rosenBrock, p, eps);
+		DHS.x.print($"Minimum found ({DHS.steps} steps):");
 	}
 }
