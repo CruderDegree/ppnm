fea803994d4a6ebcc1e83983418b74924513854f
CruderDegree <tobi2109@gmail.com>
2020-06-21 10:56:50 +0200

    Added problem 7 B

--- problems/07-roots/main.cs
+++ problems/07-roots/main.cs
@@ -1,6 +1,8 @@
 using static System.Console;
 using System;
+using System.IO;
 using static Roots;
+using static Ode;
 
 public class main{
 	static void Main(){
@@ -28,5 +30,23 @@ public class main{
 		p.print("Starting point: ");
 		roots = newton(f, p);
 		roots.print("Found solution: ");
+
+		WriteLine("\n\n--- Part B ---");
+		double rmax = 8;
+		Func<vector, vector> M = (x) => new vector(f_E(x[0],rmax));
+		p = new vector(-2.0);
+		roots = newton(M, p);
+		WriteLine($"For rmax = {rmax} we get an energy estimate of {roots[0]},\ncompared to the analytical result of -1/2");
+		using(StreamWriter sw = new StreamWriter("data.txt")){
+			for(double r = 0; r <= rmax; r += 1.0/64) sw.WriteLine($"{r}\t{f_E(roots[0],r)}");
+		}
+	}
+
+	static double f_E(double E, double r){
+		Func<double, vector, vector> swave = (x, y) => new vector(y[1], -2*(1/x + E) * y[0]);
+		double rmin = 1e-3;
+		vector frmin = new vector(rmin*(1-rmin), 1 - 2*rmin);
+		AdaptOdeSolver solver = new AdaptOdeSolver(swave, rmin, frmin, r, 1e-3, 1e-3, 1e-3);
+		return solver.yData[solver.yData.Count - 1][0];
 	}
 }
--- problems/07-roots/test.cs
+++ problems/07-roots/test.cs
@@ -4,12 +4,19 @@ using static Roots;
 
 public class test{
 	static void Main(){
+		WriteLine("Testing to find roots of f(x,y) = (x*x - 9, 81 - y*y)");
 		Func<vector, vector> f = (x) => new vector(x[0]*x[0] - 9, 81 - x[1]*x[1]);
 		vector p = new vector(10, 20);
 		vector roots = newton(f, p);
 		roots.print("Found solution: ");
 		p = new vector(-10, -20);
 		roots = newton(f,p);
-		roots.print("Found second solution?: ");
+		roots.print("Found second solution: ");
+		p = new vector(-13, 27);
+		roots = newton(f,p);
+		roots.print("Found third solution: ");
+		p = new vector(42, -65);
+		roots = newton(f,p);
+		roots.print("Found fourth solution: ");
 	}
 }
